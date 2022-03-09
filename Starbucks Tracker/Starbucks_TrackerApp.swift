63af471edd99e303fb618d13a1e3b9a868e9cc41
shashank-indukuri <shashank.indukuri05@gmail.com>
2022-03-09 12:15:03 -0600

    Initial Commit

---
+++ Starbucks Tracker/Starbucks_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starbucks_TrackerApp.swift
+//  Starbucks Tracker
+//
+//  Created by Shashank Indukuri on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Starbucks_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
